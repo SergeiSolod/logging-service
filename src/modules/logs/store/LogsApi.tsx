8d56fe53432e4521d5c47335c0a197536dbc75d1
Sergei Solod <48413787+SergeiSolod@users.noreply.github.com>
2024-03-02 18:49:02 +0300

    Update LogsApi.tsx

--- src/modules/logs/store/LogsApi.tsx
+++ src/modules/logs/store/LogsApi.tsx
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-export const API_URL = `http://localhost:5000/auth`;
-
 const $api = axios.create({
   withCredentials: true,
-  baseURL: API_URL,
+  baseURL: import.meta.env.VITE_API,
 });
 
 $api.interceptors.request.use((config) => {
